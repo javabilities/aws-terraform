3006ac9d1c602b0a10a828493cd9e21501d25428
meinholz <meinholz@javabilities.com>
2018-11-18 21:08:57 -0500

    Adding iam.

---
+++ iam/setup.tf
@@ -0,0 +1,147 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "javabilities-tfstate"
+    key    = "iam/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# resource "aws_iam_account_password_policy" "approved" {
+#   minimum_password_length        = 8
+#   require_lowercase_characters   = true
+#   require_numbers                = true
+#   require_uppercase_characters   = true
+#   require_symbols                = true
+#   allow_users_to_change_password = true
+# }
+
+resource "aws_iam_user" "audit" {
+  name = "audit"
+}
+
+resource "aws_iam_user" "operator" {
+  name = "operator"
+}
+
+variable "read_only_users" {
+  type        = "list"
+  description = "Users that are allowed to assume ReadOnly rights"
+
+  default = [
+    "audit",
+  ]
+}
+
+variable "admin_users" {
+  type        = "list"
+  description = "Users that are allowed to assume full Admin rights"
+
+  default = [
+    "operator",
+  ]
+}
+
+# trick to get users AWS Account Number
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "read_only" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "read_only" {
+  name               = "read_only"
+  description        = "ReadOnly Access"
+  assume_role_policy = "${data.aws_iam_policy_document.read_only.json}"
+}
+
+data "aws_iam_policy_document" "admin" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    effect = "Allow"
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultifactorAuthPresent"
+      values   = ["true"]
+    }
+  }
+}
+
+resource "aws_iam_role" "admin" {
+  name               = "admin"
+  description        = "Administrator Access"
+  assume_role_policy = "${data.aws_iam_policy_document.admin.json}"
+}
+
+data "aws_iam_policy_document" "assume_read_only" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = ["${aws_iam_role.read_only.arn}"]
+  }
+}
+
+resource "aws_iam_policy" "assume_read_only" {
+  name        = "assume_read_only"
+  description = "Ability to assume the read_only role"
+
+  policy = "${data.aws_iam_policy_document.assume_read_only.json}"
+}
+
+data "aws_iam_policy_document" "assume_admin" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = ["${aws_iam_role.admin.arn}"]
+  }
+}
+
+resource "aws_iam_policy" "assume_admin" {
+  name        = "assume_admin"
+  description = "Ability to assume the admin role"
+
+  policy = "${data.aws_iam_policy_document.assume_admin.json}"
+}
+
+resource "aws_iam_policy_attachment" "read_only_policy_attachment" {
+  name       = "read_only_policy_attachment"
+  roles      = ["${aws_iam_role.read_only.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_policy_attachment" "admin_policy_attachment" {
+  name       = "admin_policy_attachment"
+  roles      = ["${aws_iam_role.admin.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_policy_attachment" "assume_read_only_policy_attachment" {
+  name       = "assume_read_only_policy_attachment"
+  users      = "${var.read_only_users}"
+  policy_arn = "${aws_iam_policy.assume_read_only.arn}"
+}
+
+resource "aws_iam_policy_attachment" "assume_admin_policy_attachment" {
+  name       = "assume_admin_policy_attachment"
+  users      = "${var.admin_users}"
+  policy_arn = "${aws_iam_policy.assume_admin.arn}"
+}
